024f7b010acebe973f6976894b3f3423c1a6013f
kickscondor <kicks@kickscondor.com>
2018-07-13 08:22:24 -0600

    No need to pass transformers as an Array.

--- lib/jekyll/generators/gather_webmentions.rb
+++ lib/jekyll/generators/gather_webmentions.rb
@@ -33,17 +33,15 @@ module Jekyll
         require 'sanitize'
         @sanitize_config = Sanitize::Config::BASIC.merge(
           remove_contents: true,
-          transformers: [
+          transformers: lambda {|env|
             # Remove empty elements
-	    proc {|env|
-	      node = env[:node]
-	      return unless node.elem?
+            node = env[:node]
+            return unless node.elem?
 
-	      unless node.children.any?{|c| !c.text? || c.content.strip.length > 0 }
-		node.unlink
-	      end
-	    }
-          ])
+            unless node.children.any?{|c| !c.text? || c.content.strip.length > 0 }
+              node.unlink
+            end
+          })
       end
 
       Jekyll::WebmentionIO.log "msg", "Beginning to gather webmentions of your posts. This may take a while."
